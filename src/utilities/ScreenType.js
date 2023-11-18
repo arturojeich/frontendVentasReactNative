031f68889b138323f1d8e3d633c7260ccb6da8dd
arturojeich <arturojeich@gmail.com>
2023-11-18 01:37:13 -0300

    Removed unnecessary icon references

--- src/utilities/ScreenType.js
+++ src/utilities/ScreenType.js
@@ -1,32 +1,26 @@
 export const screenType = {
   'List Categories': {
     name: 'Listar Categorías',
-    iconName: 'format-list-bulleted',
     location: require('../screens/categories/ListCategories')
   },
   'Create Category': {
     name: 'Crear Categoría',
-    iconName: 'add',
     location: require('../screens/categories/CreateCategory')
   },
   'Edit Category': {
     name: 'Editar Categoría',
-    iconName: 'add',
     location: require('../screens/categories/EditCategory')
   },
   'List Clients': {
     name: 'Listar Clientes',
-    iconName: 'format-list-bulleted',
     location: require('../screens/clients/ListClients')
   },
   'Create Client': {
     name: 'Crear Cliente',
-    iconName: 'add',
     location: require('../screens/clients/CreateClient')
   },
   'Edit Client': {
     name: 'Editar Cliente',
-    iconName: 'add',
     location: require('../screens/clients/EditClient')
   },
   'List Products': {
